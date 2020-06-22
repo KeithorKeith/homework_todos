b118bd7888370a9c6b551a19adb1f3407289cae7
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-06-22 18:11:04 +0100

    added the list functionality.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
     new Vue({
-        el: "#app"
+        el: "#app",
+        data: {
+            todos: [
+                "Buy shopping", "Clean bathroom", "Car's MOT"
+              ]
+        }
     })
 })
