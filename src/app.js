5fcc7fdc4585c5558375e88c1eae6832f10c15d4
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-06-22 22:09:55 +0100

    added complete functionality.

--- src/app.js
+++ src/app.js
@@ -4,11 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: "#app",
         data: {
-            todos: ["Buy shopping", "Clean bathroom", "Car's MOT"],
+            todos: [
+                {name: "Buy shopping", isCompleted: false}, 
+                {name: "Clean bathroom", isCompleted: false}, 
+                {name: "Car's MOT", isCompleted: false}
+            ],
             newTodo: ""
         },
         methods: {
-
+            saveNewTodo: function(){
+                this.todos.push({
+                    name: this.newTodo,
+                    isCompleted: false
+                });
+                this.newTodo = ""
+            },
+            completeTodo: function(index){
+                this.todos[index].isCompleted = true;
+            }
         }
-    })
-})
+    });
+});
